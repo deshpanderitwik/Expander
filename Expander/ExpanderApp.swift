6d0974d29b1c1440dd56c7c67abc808bc122be3b
deshpanderitwik <deshpande.ritwik@gmail.com>
2025-09-13 15:02:41 -0700

    Initial Commit

---
+++ Expander/ExpanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpanderApp.swift
+//  Expander
+//
+//  Created by Rit on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpanderUITests/ExpanderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ExpanderUITestsLaunchTests.swift
+//  ExpanderUITests
+//
+//  Created by Rit on 9/13/25.
+//
+
+import XCTest
+
+final class ExpanderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
